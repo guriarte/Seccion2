8c669778057b195e4d7309b7e2aa77412128d5f5
German Uriarte <german.uriarte@globant.com>
2016-06-13 09:29:05 -0300

    Generacion de numeros primos

---
+++ header.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <ctime>
+#include <random>
+
+void Seccion2Ejercicio1();
+int getNumeroPrimo(int , int );
+int generarNumeroRandomRango(int , int );
+bool esPrimo(int );
