099c772cb5eee47e145151b7ab56ffbf8df7f94a
German <mryerman@gmail.com>
2016-06-12 22:12:42 -0300

    Numeros primos

--- Seccion2Ejercicio1.cpp
+++ Seccion2Ejercicio1.cpp
@@ -1,9 +1,10 @@
-/* Programar un encriptador de palabras mediante el método RSA, o dos métodos de
-encriptación simples (en caso de no querer usar el RSA); donde dado un programa, se
-muestre al mismo encriptado, con la opción de recuperar el mensaje original.
+/* Programar un encriptador de palabras mediante el mÃ©todo RSA, o dos mÃ©todos de
+encriptaciÃ³n simples (en caso de no querer usar el RSA); donde dado un programa, se
+muestre al mismo encriptado, con la opciÃ³n de recuperar el mensaje original.
 */
 
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
@@ -11,11 +12,39 @@ void Seccion2Ejercicio1()
 {
     int primo1, primo2, n, phi;
 
-    primo1 = getNumeroPrimo();
-    primo2 = getNumeroPrimo();
+    do
+    {
+        primo1 = getNumeroPrimo(100, 900);              //Obtengo los 2 primos factores de n
+        primo2 = getNumeroPrimo(100, 900);
+
+    }while(primo1 != primo2);
 
     n = primo1*primo2;
 
-    phi = (primo1-1)*(primo2-1);
+    phi = (primo1-1)*(primo2-1);                        //calculo el phi(n)
 }
 
+int getNumeroPrimo(int min, int max)                    //funcion que devuelve un numero primo entre determinado rango
+{
+    int i, numeroPrimo;
+
+    srand(time(0));
+
+    do
+    {
+        numeroPrimo = generarNumeroRandomRango(min, max);
+
+    }while(!(esPrimo(numeroPrimo));
+}
+
+int generarNumeroRandomRango(int min, int max)          //funcion que devuelve un numero random entre determinado rango
+{
+    int n = max - min + 1;
+    int resto = RAND_MAX % n;
+    int x;
+    do{
+        x = rand();
+    }while (x >= RAND_MAX - resto);
+
+    return min + x % n;
+}
